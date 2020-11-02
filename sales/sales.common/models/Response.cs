0883a84ad21e521c37ca1242649aae3a6aa52d10
GEORGEORWEL <72884980+GEORGEORWEL@users.noreply.github.com>
2020-11-02 20:13:21 +0000

    sales

---
+++ sales/sales.common/models/Response.cs
@@ -0,0 +1,13 @@
+﻿
+
+namespace sales.common.models
+{
+    public class Response
+    {
+        public bool IsSuccess { get; set; }
+
+        public string Message { get; set; }
+
+        public object Result { get; set; }
+    }
+}
